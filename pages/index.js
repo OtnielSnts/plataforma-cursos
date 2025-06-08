5ce06c05776c14bf2111e3daa9d359a2bea9dd27
OtnielSnts <73910268+OtnielSnts@users.noreply.github.com>
2025-06-07 20:47:26 -0400

    Add aula page with video playback

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <div>
       <h1>Bem-vindo ao Plataforma Cursos</h1>
+      <p>
+        <Link href="/aula">Ir para a aula</Link>
+      </p>
     </div>
   )
 }
